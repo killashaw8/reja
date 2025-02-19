ae497a1ea354e33cca39d2e50a7ca1b3f9cfa58c
killashaw8 <iamhalimov@gmail.com>
2025-02-19 19:57:47 +0900

    018-019. Express framework web server  & EJS framework frontend

--- server.js
+++ server.js
@@ -20,12 +20,13 @@ app.set("view engine", "ejs");
 
 
 //4: Routing code
-app.get("/hello", function (req, res) {
-    res.end(`<h1 style="color: red">Hello World by Romeo</h1>`);
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "Success"});
 });
 
-app.get("/gift", function (req, res) {
-    res.end(`<h1>Siz sovg'alar sahifasidasiz</h1>`);
+app.get("/", function (req, res) {
+    res.render('harid');
 });
 
 const server = http.createServer(app);
--- train.js
+++ train.js
@@ -1,1 +1,39 @@
-//A
+
+
+
+
+// 018. Express framework and building web server
+
+    // console.log("Web serverni boshlash");
+    // const express = require("express");
+    // const app = express();
+    // const http = require("http");
+
+    // //1: Kirish code
+    // app.use(express.static("public"));                   // Public folderlar browser requesti uchun ochiq
+    // app.use(express.json());                             // JSON formatdagi malumotlarni object sifatida qabul qilish
+    // app.use(express.urlencoded({ extended: true }));     // Formdan kelgan malumotlarni qabul qilish
+
+
+
+    // //2: Session code
+
+    // //3: Views code
+    // app.set("views", "views");
+    // app.set("view engine", "ejs");
+
+
+    // //4: Routing code
+    // app.get("/hello", function (req, res) {
+    //     res.end(`<h1 style="color: red">Hello World by Romeo</h1>`);
+    // });
+
+    // app.get("/gift", function (req, res) {
+    //     res.end(`<h1>Siz sovg'alar sahifasidasiz</h1>`);
+    // });
+
+    // const server = http.createServer(app);
+    // let port = 3000;
+    // server.listen(port, function () {
+    //     console.log(`The server is running successfully on port ${port}`);
+    // })
